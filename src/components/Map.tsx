c6e268b6db8e5d0618735b0cabab97d01ec9ce28
octaviookumu <octaviookumu@gmail.com>
2026-04-29 15:17:52 +0300

    feat: add maptiler to use a darker map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,9 +1,11 @@
 import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
+import { MaptilerLayer } from "@maptiler/leaflet-maptilersdk";
 import { CoordsContext } from "../context/map-context";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
+const MAPTILER_KEY = import.meta.env.VITE_MAPTILER_KEY;
 
 export default function Map() {
   const {
@@ -18,11 +20,9 @@ export default function Map() {
       style={{ height: "500px", width: "1000px" }}
     >
       <MapClick lat={lat} lon={lon} />
+      <MapTileLayer />
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <TileLayer
+        opacity={0.7}
         url={`https://tile.openweathermap.org/map/${mapType}/{z}/{x}/{y}.png?appid=${API_KEY}`}
       />
       <Marker position={[lat, lon]} />
@@ -42,6 +42,25 @@ function MapClick({ lat, lon }: { lat: number; lon: number }) {
 
     onMapClick(lat, lng);
   });
+
+  return null;
+}
+
+function MapTileLayer() {
+  const map = useMap();
+
+  useEffect(() => {
+    const tileLayer = new MaptilerLayer({
+      style: "basic-dark",
+      apiKey: MAPTILER_KEY,
+    });
+    tileLayer.addTo(map);
+
+    // have cleanup when working with libraries that manipulate the DOM directly
+    return () => {
+      map.removeLayer(tileLayer);
+    };
+  }, [map]);
 
   return null;
 }
