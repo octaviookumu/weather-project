72be611f4cb3ad47eb183cac192909e764e6fce5
octaviookumu <octaviookumu@gmail.com>
2026-04-30 16:54:41 +0300

    feat: fade in animation for cards after skeleton loader

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -1,4 +1,5 @@
 import { type ReactNode } from "react";
+import clsx from "clsx";
 
 type Props = {
   children: ReactNode;
@@ -10,7 +11,14 @@ export default function Card({ children, title, childrenClassName }: Props) {
   return (
     <div className="p-4 rounded-xl bg-linear-br from-card to-card/60 shadow-md flex flex-col gap-4">
       <h2 className="text-2xl font-semibold">{title}</h2>
-      <div className={childrenClassName}>{children}</div>
+      <div
+        className={clsx(
+          childrenClassName,
+          "animate-[fade-in_1s_ease-out_forwards]",
+        )}
+      >
+        {children}
+      </div>
     </div>
   );
 }
