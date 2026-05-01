768253cc3219f75d8b67a63ec2bb0eca8dcb7a48
octaviookumu <octaviookumu@gmail.com>
2026-05-02 02:30:23 +0300

    feat: add tooltip

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -5,6 +5,8 @@ import { Suspense, useContext } from "react";
 import Card from "./cards/Card";
 import { Slider } from "./ui/slider";
 import clsx from "clsx";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+import Information from "/src/assets/information.svg?react";
 
 export default function SidePanel() {
   return (
@@ -28,7 +30,21 @@ function AirPollution() {
     <div className="flex flex-col gap-4">
       <h1 className="text-2xl font-semibold">Air Pollution</h1>
       <h1 className="text-5xl font-semibold">{data.list[0].main.aqi}</h1>
-      <h1 className="text-2xl font-semibold">AQI</h1>
+      <div className="flex items-center gap-2">
+        <h1 className="text-2xl font-semibold">AQI</h1>
+        <Tooltip>
+          <TooltipTrigger>
+            <Information className="size-4" />
+          </TooltipTrigger>
+          <TooltipContent className="z-2000">
+            <p className="max-w-xs">
+              {" "}
+              Air Quality Index. Possible values: 1, 2, 3, 4, 5. Where 1 = Good,
+              2 = Fair, 3 = Moderate, 4 = Poor, 5 = Very Poor.
+            </p>
+          </TooltipContent>
+        </Tooltip>
+      </div>
       <div>
         {Object.entries(data.list[0].components).map(([key, value]) => {
           const pollutant =
@@ -74,7 +90,20 @@ function AirPollution() {
               className="hover:scale-105 transition-transform duration-300 from-sidebar-accent to-sidebar-accent/60 gap-0!"
             >
               <div className="flex justify-between">
-                <span className="text-lg font-bold capitalize ">{key}</span>
+                <div className="flex items-center gap-2">
+                  <span className="text-lg font-bold capitalize">{key}</span>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <Information className="size-4" />
+                    </TooltipTrigger>
+                    <TooltipContent className="z-2000">
+                      <p className="max-w-xs">
+                        Concentration of{" "}
+                        {pollutantNameMapping[key.toUpperCase() as Pollutant]}
+                      </p>
+                    </TooltipContent>
+                  </Tooltip>
+                </div>
                 <span className="text-lg font-semibold">{value}</span>
               </div>
               <Slider min={0} max={max} value={[value]} disabled />
@@ -83,8 +112,9 @@ function AirPollution() {
                 <p>{max}</p>
               </div>
               <div className="flex justify-between">
-                {Object.keys(pollutant).map((quality) => (
+                {Object.keys(pollutant).map((quality, i) => (
                   <span
+                    key={i}
                     className={clsx(
                       "px-2 py-1 rounded-md text-xs font-medium",
                       quality === currentLevel
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <TooltipProvider>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </TooltipProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
