353cd39a320bf5752f613022d25c23a0c57cd5b8
octaviookumu <octaviookumu@gmail.com>
2026-05-04 01:48:13 +0300

    fix: match skeleton loader classes

--- src/components/skeletons/DailySkeleton.tsx
+++ src/components/skeletons/DailySkeleton.tsx
@@ -3,7 +3,10 @@ import { Skeleton } from "../ui/skeleton";
 
 export default function DailySkeleton() {
   return (
-    <Card title="Daily Forecast" childrenClassName="flex flex-col gap-4">
+    <Card
+      title="Daily Forecast"
+      childrenClassName="flex flex-col gap-4 2xl:justify-between"
+    >
       {/* instead of mapping over data, we can map through an array of 8 items to
       show 8 skeletons for the 8 days of the forecast */}
       {Array.from({ length: 8 }).map((_, index) => (
